32b20eec51b923ef46173843f4c9b133346d9f7b
Ante Koceić <ante@crocoder.dev>
2026-03-18 16:31:38 +0100

    Removed unused env variable

--- src/app/env.d.ts
+++ src/app/env.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     RESEND_API_KEY: string;
     RESEND_SEGMENT_ID: string;
-    NEWSLETTER_ADMIN_SECRET: string;
     FORM_EMAIL_FROM: string;
     FORM_EMIAL_TO: string;
   }
